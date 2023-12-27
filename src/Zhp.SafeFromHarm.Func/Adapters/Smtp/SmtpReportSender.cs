db15c182b864b5ff3966700ac35fc4d4bb8e321b
Karol Grodzicki <karol.gro@gmail.com>
2023-12-27 09:33:12 +0100

    Add attachment

--- src/Zhp.SafeFromHarm.Func/Adapters/Smtp/SmtpReportSender.cs
+++ src/Zhp.SafeFromHarm.Func/Adapters/Smtp/SmtpReportSender.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
 using MimeKit;
+using System.Text;
 using Zhp.SafeFromHarm.Domain.Model.CertificationNotifications;
 using Zhp.SafeFromHarm.Domain.Ports.CertificationNotifications;
 
@@ -25,15 +26,39 @@ internal class SmtpReportSender(
                 Oto raport z wykonywania szkoleń Safe from Harm dla Twojej chorągwi. Raporty zostały również przesłane poszczególnym hufcom.
                 Z harcerskim pozdrowieniem,
                 Zespół Safe from Harm
-                """
+                """,
+            Attachments =
+            {
+                new MimePart(System.Net.Mime.MediaTypeNames.Text.Csv)
+                {
+                    Content = new MimeContent(BuildReport(entries)),
+                    ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                    ContentTransferEncoding = ContentEncoding.Base64,
+                    FileName = $"Raport SFH-{DateTime.Today:yyyy-MM-dd}-{unit.Name}.csv"
+                }
+            }
         };
 
         var mail = new MimeMessage(
             from: new[] { new MailboxAddress("Safe from Harm", options.Username) },
             to: new[] { new MailboxAddress(unit.Name, recipientMail) },
-            "Raport chorągwiany ze szkoleń Safe from Harm",
+            $"Raport chorągwiany ze szkoleń Safe from Harm - {unit.Name}",
             bodyBuilder.ToMessageBody());
 
         await client.SendAsync(mail);
+    }
+
+    private static MemoryStream BuildReport(IEnumerable<CertificationReport.ReportEntry> entries)
+    {
+        var stream = new MemoryStream();
+        using var writer = new StreamWriter(stream, new UTF8Encoding(true), leaveOpen: true);
+
+        writer.WriteLine("Członek;Numer ewidencji;Hufiec;Data certyfikacji");
+        foreach (var entry in entries)
+        {
+            writer.WriteLine($"{entry.Member.FirstName} {entry.Member.LastName};{entry.Member.MembershipNumber};{entry.Member.Supervisor.Name};{entry.CertificationDate}");
+        }
+
+        return stream;
     }
 }
